98e6727c11ed03fef58d9a5177fb40f4aaa6cd33
Pankil Chhabra <pankilchhabra@gmail.com>
2024-01-26 00:33:08 +0530

    Vercel hosting changes

--- office_emp_mgmt/settings.py
+++ office_emp_mgmt/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b+zxcqkk$$krztrffocl+c8+xz&)%x&81o#=m(jr39t*ajyykp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static work
+
+import os 
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
